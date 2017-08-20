a7244c8d07846098db210f88eabb1005c28c70d7
Shuiloon Yip <shuoodle@gmail.com>
2017-08-20 16:29:46 +0100

    Add mlab database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.DB_URL);
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
