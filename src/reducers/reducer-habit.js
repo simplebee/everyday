46bd1d39eefca0bdd8f5a052f1e8dac685652efa
Shuiloon Yip <shuoodle@gmail.com>
2017-10-30 19:39:42 +0000

    Add fetch habit to habit reducer

--- src/reducers/reducer-habit.js
+++ src/reducers/reducer-habit.js
@@ -1,13 +1,27 @@
-import { FETCH_HABITS, CREATE_HABIT } from '../actions/types';
+import { FETCH_HABITS, CREATE_HABIT, FETCH_HABIT } from '../actions/types';
 
 function habit(state = [], action) {
   switch (action.type) {
     case FETCH_HABITS:
       return action.payload;
     case CREATE_HABIT:
-      return [...state, action.payload]
+      return [...state, action.payload];
+    case FETCH_HABIT:
+      return addOrUpdateHabit(state, action);
     default:
       return state;
+  }
+}
+
+function addOrUpdateHabit(state, action) {
+  const newState = [...state];
+  const { payload } = action;
+  const index = newState.findIndex(obj => obj._id === payload._id);
+  if (index === -1) {
+    return [...state, payload];
+  } else {
+    newState[index] = payload;
+    return newState;
   }
 }
 
