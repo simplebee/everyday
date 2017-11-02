cd32d693137e70f03b77a7f42e25e778935a93c3
Shuiloon Yip <shuoodle@gmail.com>
2017-11-02 16:19:46 +0000

    Add calender view

--- src/components/habit-show.js
+++ src/components/habit-show.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import { fetchHabit } from '../actions';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 class HabitShow extends Component {
   
@@ -9,15 +12,53 @@ class HabitShow extends Component {
     this.props.fetchHabit(habitId);
   }
 
-  render() {
+  renderValue(day) {
+    if (!this.props.habit) return;
+
+    const { datapoints } = this.props.habit;
+    day.setHours(0, 0, 0, 0); // 'day' is originally set at local time 12pm
+    const datapoint = datapoints.find(datapoint => {
+      const datapointDate = moment(datapoint.date).toDate(); // moment() to create local date. Not using new Date('string'), as it returns UTC date
+      return datapointDate.getTime() === day.getTime();
+    });
+    if (datapoint) return <div>{datapoint.value}</div>;
+  }
+
+  renderDay = (day) => {
+    const date = day.getDate();
     return (
-      <div>{this.props.match.params.habitId}</div>
+      <div>
+        {date}
+        <div>
+          {this.renderValue(day)}
+        </div>
+      </div>
     );
   }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <DayPicker
+            month={new Date()}
+            renderDay={this.renderDay}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state, ownProps) {
+  const { habitId } = ownProps.match.params;
+  const { habit } = state;
+  const index = habit.findIndex(obj => obj._id === habitId);
+  return { habit: habit[index] };
 }
 
 const mapDispatchToProps = {
   fetchHabit
 };
 
-export default connect(null, mapDispatchToProps)(HabitShow);
+export default connect(mapStateToProps, mapDispatchToProps)(HabitShow);
