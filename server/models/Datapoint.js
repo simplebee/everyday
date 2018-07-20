1ed88f3275119ca110380e581f3b01379b5ca0dc
Shuiloon Yip <shuoodle@gmail.com>
2018-07-20 13:42:00 +0100

    Change date from string to date obj and add timestamps

--- server/models/Datapoint.js
+++ server/models/Datapoint.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const dataPointSchema = new Schema({
-  date: String,
-  value: Number
-});
+const dataPointSchema = new Schema(
+  {
+    date: { type: Date, default: Date.now },
+    value: Number
+  },
+  { timestamps: true }
+);
 
 const Datapoint = mongoose.model('Datapoint', dataPointSchema);
 
--- server/models/Habit.js
+++ server/models/Habit.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const habitSchema = new Schema({
-  name: String,
-  startDate: String,
-  goalValue: Number,
-  frequency: String,
-  datapoints: [{ type: Schema.Types.ObjectId, ref: 'Datapoint' }]
-});
+const habitSchema = new Schema(
+  {
+    name: String,
+    startDate: Date,
+    goalValue: Number,
+    frequency: String,
+    datapoints: [{ type: Schema.Types.ObjectId, ref: 'Datapoint' }]
+  },
+  { timestamps: true }
+);
 
 const Habit = mongoose.model('Habit', habitSchema);
 
