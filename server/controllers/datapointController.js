d1ec9b9d93f2e8bdc4cd552ab11d4cd077aa20f2
Shuiloon Yip <shuoodle@gmail.com>
2018-07-20 13:27:49 +0100

    In create and update datapoint routes, remove adding values

--- server/controllers/datapointController.js
+++ server/controllers/datapointController.js
@@ -12,48 +12,18 @@ exports.index = function(req, res) {
 
 exports.create = function(req, res) {
   const { habitId } = req.params;
-  const { date } = req.body;
-  const { value } = req.body;
 
   Habit.findById(habitId)
     .exec()
-    .then(habit => {
-      return Datapoint.find()
-        .where('_id')
-        .in(habit.datapoints)
-        .where('date')
-        .equals(date)
-        .exec(addOrUpdateDatapoint);
-    });
-
-  function addOrUpdateDatapoint(err, data) {
-    if (err) console.error(err);
-    if (Array.isArray(data) && !data.length) {
-      Datapoint.create(req.body, function(err, datapointData) {
-        if (err) console.error(err);
-        Habit.findByIdAndUpdate(
-          habitId,
-          { $push: { datapoints: datapointData._id } },
-          { new: true },
-          function(err, habitData) {
-            if (err) console.error(err);
-            res.send(datapointData);
-          }
-        );
+    .then(habitDoc => {
+      Datapoint.create(req.body).then(datapointDoc => {
+        habitDoc.datapoints.push(datapointDoc._id);
+        habitDoc.save().then(() => {
+          res.json(datapointDoc);
+        });
       });
-    } else {
-      const newValue = data[0].value + value;
-      Datapoint.findByIdAndUpdate(
-        data[0]._id,
-        { $set: { value: newValue } },
-        { new: true },
-        function(err, datapointData) {
-          if (err) console.error(err);
-          res.send(datapointData);
-        }
-      );
-    }
-  }
+    })
+    .catch(err => console.error(err));
 };
 
 exports.show = function(req, res) {
@@ -65,32 +35,11 @@ exports.show = function(req, res) {
 };
 
 exports.update = function(req, res) {
-  const { habitId } = req.params;
   const { datapointId } = req.params;
-  const { date } = req.body;
-
-  isSameDatapointDate(datapointId, date).then(isSameDate => {
-    if (isSameDate) {
-      updateDatapoint(datapointId, req.body).then(datapoint =>
-        res.json(datapoint)
-      );
-    } else {
-      isUniqueDatapointDate(habitId, date).then(dateIsUnique => {
-        if (dateIsUnique) {
-          updateDatapoint(datapointId, req.body).then(datapoint =>
-            res.json(datapoint)
-          );
-        } else {
-          res.status(400).send({
-            error: {
-              code: 101,
-              message: 'Date already exist'
-            }
-          });
-        }
-      });
-    }
-  });
+  Datapoint.findByIdAndUpdate(datapointId, { $set: req.body }, { new: true })
+    .exec()
+    .then(datapoint => res.json(datapoint))
+    .catch(err => console.error(err));
 };
 
 exports.destroy = function(req, res) {
@@ -109,43 +58,3 @@ exports.destroy = function(req, res) {
     })
     .catch(err => console.error(err));
 };
-
-function isSameDatapointDate(datapointId, date) {
-  return Datapoint.findById(datapointId)
-    .exec()
-    .then(datapoint => {
-      if (datapoint.date === date) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-}
-
-function isUniqueDatapointDate(habitId, date) {
-  return Habit.findById(habitId)
-    .exec()
-    .then(habit => {
-      return Datapoint.find()
-        .where('_id')
-        .in(habit.datapoints)
-        .where('date')
-        .equals(date)
-        .exec()
-        .then(datapoint => {
-          if (Array.isArray(datapoint) && !datapoint.length) {
-            return true;
-          } else {
-            return false;
-          }
-        });
-    });
-}
-
-function updateDatapoint(datapointId, updateData) {
-  return Datapoint.findByIdAndUpdate(
-    datapointId,
-    { $set: updateData },
-    { new: true }
-  ).exec();
-}
